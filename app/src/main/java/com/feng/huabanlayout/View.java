601ab2a2789c66b41566641a7f9797089c667ac6
lm11000 <822421@qq.com>
2016-08-17 18:35:45 +0800

    画了矢量图的椭圆背景

---
+++ app/src/main/java/com/feng/huabanlayout/View.java
@@ -0,0 +1,15 @@
+package com.feng.huabanlayout;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+
+/**
+ * Created by wicroinfo001 on 2016/8/17.
+ */
+public class View extends android.view.View{
+Canvas canvas;
+    public View(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
